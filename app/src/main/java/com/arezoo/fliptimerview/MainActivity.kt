1b2af16f056d2649c0058fbf78dfa40edd29e812
Arezoo Nazer <arezoo.nazer@cafebazaar.ir>
2021-09-17 11:26:11 +0430

    Add FlipTimerView to MainActivity

--- app/src/main/java/com/arezoo/fliptimerview/MainActivity.kt
+++ app/src/main/java/com/arezoo/fliptimerview/MainActivity.kt
@@ -2,10 +2,34 @@ package com.arezoo.fliptimerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.arezoo.fliptimerview.fliptimer.CounterDownCallback
+import com.arezoo.fliptimerview.fliptimer.FlipTimerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initFlipTimer()
+    }
+
+    private fun initFlipTimer() {
+        findViewById<FlipTimerView>(R.id.flipTimerView).apply {
+            startCountDown(REMAINING_TIME_MILLI,
+                object : CounterDownCallback {
+                    override fun countdownAboutToFinish() {
+                        super.countdownAboutToFinish()
+                    }
+
+                    override fun countdownFinished() {
+                        TODO("Not yet implemented")
+                    }
+                })
+        }
+    }
+
+    companion object {
+
+        private const val REMAINING_TIME_MILLI = 720000L
     }
 }
